237feff8418d4ce69c287aaf16a31d7e6e64c0ee
risfy <14286113+risbah@users.noreply.github.com>
2020-05-09 11:49:25 -0400

    Changed Main.java: removed my previous code there; 1st commit after changing github setup

--- src/main/java/com/javalearners/libraryapp/Main.java
+++ src/main/java/com/javalearners/libraryapp/Main.java
@@ -9,33 +9,7 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        String sql = "CREATE TABLE TEST " +
-                "(id INTEGER not NULL, " +
-                " first VARCHAR(255), " +
-                " last VARCHAR(255), " +
-                " age INTEGER, " +
-                " PRIMARY KEY ( id ))";
-        String sql2 = "INSERT INTO TEST " +
-                "VALUES (100, 'Zara', 'Ali', 18)";
-        System.out.println("test");
-
-        try(H2Connection connector = H2Connection.getInstance()){
-            var connection = connector.getConnection();
-            try(Statement statement = connection.createStatement()){
-                //statement.executeUpdate(sql);
-                statement.executeUpdate(sql2);
-                try(ResultSet resultSet = statement.executeQuery("SELECT * FROM TEST")){
-                    while(resultSet.next()){
-
-                        System.out.println(resultSet.getString("first"));
-                    }
-                }
-            }
-        } catch(SQLException e){
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Test code here
     }
 
 }
